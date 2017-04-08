805bcbd0c235143d3832077020edff66f1265392
NikolayNN <Horushko.Nikolay@gmail.com>
2017-04-08 23:02:08 +0300

    #7 : USR-F4 added test if request with non-existent uuid

--- src/test/java/juja/microservices/users/controller/UserControllerTest.java
+++ src/test/java/juja/microservices/users/controller/UserControllerTest.java
@@ -66,11 +66,11 @@ public class UserControllerTest {
         List<User> users = new ArrayList<>();
         users.add(new User("AAAA123", "Vasya", "Ivanoff", "vasya@mail.ru", "vasya@gmail.com", "vasya", "vasya.ivanoff",
                 "linkedin/vasya", "facebook/vasya", "twitter/vasya"));
-        Map params = new HashMap<String,String>();
-        params.put("email","vasya@mail.ru");
+        Map params = new HashMap<String, String>();
+        params.put("email", "vasya@mail.ru");
         when(service.searchUser(params)).thenReturn(users);
         String result = mockMvc.perform(get("/users/search")
-                .param("email","vasya@mail.ru")
+                .param("email", "vasya@mail.ru")
                 .contentType(APPLICATION_JSON_UTF8))
                 .andExpect(content().contentType(APPLICATION_JSON_UTF8))
                 .andExpect(status().isOk())
@@ -90,5 +90,18 @@ public class UserControllerTest {
                 .andExpect(status().isOk())
                 .andReturn().getResponse().getContentAsString();
         assertThatJson(result).isEqualTo(user);
+    }
+
+    @Test
+    public void shouldThrowBadRequestIfNonExistentUuid() throws Exception {
+        when(service.searchUser("nonExistentUuid")).thenThrow(new UserException("No users found by your request!"));
+        String result = mockMvc.perform(get("/users/nonExistentUuid")
+                .contentType(APPLICATION_JSON_UTF8))
+                .andExpect(content().contentType(APPLICATION_JSON_UTF8))
+                .andExpect(status().isBadRequest())
+                .andReturn().getResponse().getContentAsString();
+        assertThatJson("{\"httpStatus\":400,\"internalErrorCode\":0,\"clientMessage\":\"Oops something went wrong :(\"," +
+                "\"developerMessage\":\"General exception for this service\",\"exceptionMessage\":\"No users found by your request!\"," +
+                "\"detailErrors\":[]}").isEqualTo(result);
     }
 }
