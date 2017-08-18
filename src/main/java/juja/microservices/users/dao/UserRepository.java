8fde6d83a3ab7e863c821238bf66cbe0bc409886
Vadim Dyachenko <vadim.dyachenko@gmail.com>
2017-08-18 14:22:16 +0300

    #44 Removed CRMRepository

--- src/main/java/juja/microservices/users/dao/UserRepository.java
+++ src/main/java/juja/microservices/users/dao/UserRepository.java
@@ -1,16 +1,18 @@
 package juja.microservices.users.dao;
 
 import juja.microservices.users.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.UUID;
 
 /**
  * @author Denis Tantsev (dtantsev@gmail.com)
  * @author Olga Kulykova
+ * @author Vadim Dyachenko
  */
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User, UUID> {
     List<User> getAllUsers();
     User getUserBySlack(String slack);
     User getUserByUuid(String uuid);
-    User getUserById(String id);
 }
